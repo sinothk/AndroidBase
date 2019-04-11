b122738a7673a9bff80f4054287a374ff0dff744
LiangYT <381518188@qq.com>
2019-04-11 18:19:25 +0800

    同步！

--- app/src/main/java/com/sinothk/android/base/demo/AndroidBaseDemoMainActivity.java
+++ app/src/main/java/com/sinothk/android/base/demo/AndroidBaseDemoMainActivity.java
@@ -1,15 +1,22 @@
 package com.sinothk.android.base.demo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class AndroidBaseDemoMainActivity extends AppCompatActivity {
+import com.sinothk.android.all.titlebar.TitleBarBaseActivity;
+
+public class AndroidBaseDemoMainActivity extends TitleBarBaseActivity {
+
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_android_base_demo_main;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_android_base_demo_main);
+        setTitleBar("It's All ！");
 
 //        TitleBarView.getString();
     }
+
 }

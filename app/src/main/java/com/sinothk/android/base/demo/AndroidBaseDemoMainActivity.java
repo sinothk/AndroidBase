413e15ff505c70da2c96b7eeeb9de67bc4fdfda2
LiangYT <381518188@qq.com>
2019-04-11 16:45:32 +0800

    分库

--- app/src/main/java/com/sinothk/android/base/demo/AndroidBaseDemoMainActivity.java
+++ app/src/main/java/com/sinothk/android/base/demo/AndroidBaseDemoMainActivity.java
@@ -3,11 +3,15 @@ package com.sinothk.android.base.demo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.sinothk.android.base.view.TitleBarView;
+
 public class AndroidBaseDemoMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android_base_demo_main);
+
+        TitleBarView.getString();
     }
 }

01addda531c914ffc048fb1335b90ab6bc87ef23
LiangYT <381518188@qq.com>
2019-04-12 14:55:38 +0800

    同步！

--- app/src/main/java/com/sinothk/android/base/demo/AndroidBaseDemoMain2Activity.java
+++ app/src/main/java/com/sinothk/android/base/demo/AndroidBaseDemoMain2Activity.java
@@ -1,13 +1,10 @@
 package com.sinothk.android.base.demo;
 
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.sinothk.android.all.titlebar.TitleBarBaseActivity;
-import com.sinothk.android.utils.glide.ImageLoader;
-import com.sinothk.smartImage.FaceImageUtil;
 
 /**
  * https://github.com/bumptech/glide
@@ -28,14 +25,6 @@ public class AndroidBaseDemoMain2Activity extends TitleBarBaseActivity {
         setTitleBar("Hello !");
 
         iv001 = findViewById(R.id.iv001);
-
-        ImageLoader.show(this, "https://github.com/aryarohit07/GlideFaceDetectionTransformation/blob/master/images/original_image4.jpg?raw=true", new ImageLoader.GlideLoadBitmapCallback() {
-            @Override
-            public void getBitmapCallback(Bitmap bitmap) {
-                Bitmap clipBitmap = FaceImageUtil.cropToBitmap(AndroidBaseDemoMain2Activity.this, bitmap, 100, 100);
-                iv001.setImageBitmap(clipBitmap);
-            }
-        });
     }
 
     public void goto2(View view) {
